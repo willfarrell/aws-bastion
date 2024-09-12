365b1c16b0cfecdc680df0ce4ad4c5d3cd76a077
will Farrell <willfarrell@proton.me>
2024-09-12 08:33:12 -0600

    feat: add in seelog override

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 set -e
 
+# Same as path used in seelog.xml
+# TODO Moved to /tmp to allow ECS to have `readonlyRootFilesystem: true`
+DIR=/var/log/amazon/ssm/
+
 sleep 1m
 
-CONNECT=$(cat /var/log/amazon/ssm/amazon-ssm-agent.log | grep 'Plugin Port started' | wc -l)
-DISCONNECT=$(cat /var/log/amazon/ssm/amazon-ssm-agent.log | grep 'The session was cancelled' | wc -l)
+CONNECT=$(cat ${DIR}/amazon-ssm-agent.log | grep 'Plugin Port started' | wc -l)
+DISCONNECT=$(cat ${DIR}/amazon-ssm-agent.log | grep 'The session was cancelled' | wc -l)
 
 while [ "${DISCONNECT}" -lt "${CONNECT}" ]; do
   echo $((${CONNECT} - ${DISCONNECT})) active
   sleep 1m
-  CONNECT=$(cat /var/log/amazon/ssm/amazon-ssm-agent.log | grep 'Plugin Port started' | wc -l)
-  DISCONNECT=$(cat /var/log/amazon/ssm/amazon-ssm-agent.log | grep 'The session was cancelled' | wc -l)
+  CONNECT=$(cat ${DIR}/amazon-ssm-agent.log | grep 'Plugin Port started' | wc -l)
+  DISCONNECT=$(cat ${DIR}/amazon-ssm-agent.log | grep 'The session was cancelled' | wc -l)
 done
 
-#cat /var/log/amazon/ssm/amazon-ssm-agent.log
-#cat /var/log/amazon/ssm/errors.log
+#ls -al /etc/amazon/ssm/
+#ls -al /var/log/amazon/ssm/
+#cat ${DIR}/amazon-ssm-agent.log
+#cat ${DIR}/errors.log
 
 echo "No active connections. Shutting down."
